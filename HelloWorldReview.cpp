43bdfab1d1a92bf15d31b8122b76ba59f0e0b80d
jessicagutierrez145 <Kikagutierrez419@gmail.com>
2025-08-13 11:42:41 -0700

    Add files via upload

---
+++ HelloWorldReview.cpp
@@ -0,0 +1,19 @@
+#include <iostream>//
+using namespace std ;
+
+int main() {
+    cout << "Hello World!";
+
+    int a = 3;
+    int b = 9;
+
+    cout << "a = " << a;
+    cout << "b = " << b;
+
+    int sum = a + b;
+
+    cout << "sum = " << sum;
+
+    return 0;
+
+}
