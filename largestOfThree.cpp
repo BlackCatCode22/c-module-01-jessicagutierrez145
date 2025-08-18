face278a7f858b06fa0d51084c2cca1920ecf251
jessicagutierrez145 <Kikagutierrez419@gmail.com>
2025-08-18 12:17:23 -0700

    Add files via upload

---
+++ largestOfThree.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+
+cout<< "Hello and Welcome to MY Largest of three ints program." << endl;
+
+int num1;
+int num2;
+int num3;
+int sumOfTheInts;
+
+num1 = 0;
+num2 = 0;
+num3 = 0;
+sumOfTheInts = 0;
+
+cout << "\n Please enter a value for num1: ";
+cin >> num1;
+cout << "\n You entered " << num1 << " for num1: ";
+cout << "\n Please enter a value for num1: ";
+cin >> num1;
+cout << "\n You entered " <<num1 << " for num1: ";
+cout << "\n Please enter a value for num1: ";
+cin >> num1;
+cout << "\n Please enter a value for sumOfTheInts: ";
+cout << " You entered " << num1 << " for num1: ";
+
+if (num1 < num2) {}
+cout << num1 << "is greater than " << num2;
+    } else {
+cout << num2 << "is greater than " << num1;
+}
+    return 0;
+
+
+}
